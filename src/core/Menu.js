48492c68613fba782611222535accad1cc40af3f
Kiran Kamath <kirankamath@localhost.localdomain>
2019-08-19 00:54:52 +0530

    16M late few more to go

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,22 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../auth';
 
-
 const isActive = (history, path) => {
-    if (history.location.pathname === path) {
-        return { color: '#FF0000' };
-    }
-    else {
-        return { color: '#FFFFFF' };
-    }
+    if (history.location.pathname === path) return { color: '#ff9900' };
+    else return { color: '#ffffff' };
 };
-//color is not changing in tabs 
-
-
-
-
-
 
 const Menu = ({ history }) => (
     <div>
@@ -36,19 +25,37 @@ const Menu = ({ history }) => (
                 </Link>
             </li>
 
+            <li className="nav-item">
+                <Link to={`/post/create`} style={isActive(history, `/post/create`)} className="nav-link">
+                    Create Post
+                </Link>
+            </li>
+
             {!isAuthenticated() && (
-                <>
+                <React.Fragment>
                     <li className="nav-item">
-                        <Link className="nav-link" sytle={isActive(history, '/signin')} to="/signin">Sign In</Link>
+                        <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
+                            Sign In
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" sytle={isActive(history, '/signup')} to="/signup">Sign Up</Link>
+                        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
+                            Sign Up
+                        </Link>
                     </li>
-                </>
+                </React.Fragment>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 'admin' && (
+                <li className="nav-item">
+                    <Link to={`/admin`} style={isActive(history, `/admin`)} className="nav-link">
+                        Admin
+                    </Link>
+                </li>
             )}
 
             {isAuthenticated() && (
-                <>
+                <React.Fragment>
                     <li className="nav-item">
                         <Link to={`/findpeople`} style={isActive(history, `/findpeople`)} className="nav-link">
                             Find People
@@ -66,21 +73,18 @@ const Menu = ({ history }) => (
                     </li>
 
                     <li className="nav-item">
-                        <span className="nav-link"
-                            sytle={(isActive(history, 'signup'),
-                                { cursor: "pointer", color: "#fff" })
-                            }
-                            onClick={() => signout(() => history.push('/'))}>Sign Out
+                        <span
+                            className="nav-link"
+                            style={{ cursor: 'pointer', color: '#fff' }}
+                            onClick={() => signout(() => history.push('/'))}
+                        >
+                            Sign Out
                         </span>
                     </li>
-                    
-                </>
+                </React.Fragment>
             )}
         </ul>
     </div>
 );
 
 export default withRouter(Menu);
-
-
-
